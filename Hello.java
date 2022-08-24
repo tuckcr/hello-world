d618ca873bf62a82e3a1823ff29e6cd2e3d7cbe1
tuckcr <112002641+tuckcr@users.noreply.github.com>
2022-08-24 11:56:56 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*********************************
+* Hello.java
+**********************************
+* Christopher Tuck
+* 8/24/2022
+* CMSC 255 Section 004
+**********************************/
+
+public class Hello {
+		public static void main(String[] args) {
+
+			System.out.println("<Christopher Tuck>");
+			System.out.println("<Hello.java>");
+			System.out.println("<Hello, World!>");
+			System.out.println("<8/24/2022>");
+			System.out.println("<Course 255 Section 004>");
+			System.out.println();
+		}
+}
